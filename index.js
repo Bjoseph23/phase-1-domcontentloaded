4003ba5ed40ccb73b00c2d9d5c2690ee518f65fe
+ <+>
2024-04-07 23:04:13 +0300

    DOne

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",updateDom)
+function updateDom(){
+    document.getElementById("text")
+    .textContent="This is really cool!"
+}
